26fb50aa42bb08f729b708871f3323b218e89af0
obinna <biostechnologyng@gmail.com>
2023-01-30 20:33:08 +0100

    Initial initilization

---
+++ src/app/services/services.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServicesComponent } from './services.component';
+
+@NgModule({
+  declarations: [ServicesComponent],
+  imports: [CommonModule],
+})
+export class ServicesModule {}
